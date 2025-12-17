969cfb6727a3b5647923563e38dee6818f6779f8
teesha17 <teeshu.2179@gmail.com>
2025-12-17 11:49:31 +0530

    resolved import error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import CriticalAreas from './components/CriticalAreas.jsx'
 import DeliverablesSection from './components/DeliverablesSections.jsx'
 import FinalCTASection from './components/FinalCTASection.jsx'
-import HomePage from './components/Homepage.jsx'
+import HomePage from './components/HomePage.jsx'
 import ProcessSection from './components/ProcessSection.jsx'
 
 function App() {
